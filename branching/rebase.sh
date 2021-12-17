3bfb783b44421d815604d5c75f2d01c93b4e318c
Aizada Kambarova <kambarova.ayzada@inbox.ru>
2021-12-17 13:57:19 +0300

    changes

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -4,7 +4,9 @@
 # display command line options
 
 count=1
-for param in "$*"; do
-    echo "\$* Parameter #$count = $param"
+for param in "$@"; do
+    echo "\$@ Parameter #$count = $param"
     count=$(( $count + 1 ))
-done 
+done
+
+echo "=====" 
